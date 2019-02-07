51b7e552d034d4e269a31c1b5bd32ce622ac2b92
Sergey <memonanofsa@gmail.com>
2019-02-07 22:46:37 +0500

    Add pug and refactor App.vue and Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div
+    h1 hi there
 </template>
 
 <script>
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import NumberInput from '@/components/NumberInput.vue';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
+  // components: {
+  //   NumberInput,
+  // },
 };
 </script>
