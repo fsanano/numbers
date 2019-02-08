c03472a364479c3e35b6263de65b19c8057fd68c
Sergey <memonanofsa@gmail.com>
2019-02-08 22:52:36 +0500

    add file extension

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template lang="pug">
   div(id="app")
+    //- router-view(:key="$route.fullPath")
+    TheHeader
     router-view
 </template>
 
-<style lang="stylus">
+<script>
+import TheHeader from '@/components/TheHeader.vue';
 
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style lang="stylus">
+  html
+    height 100%
+
+  body
+    height 100%
+    margin 0
+    position: relative;
+    font-family: 'Special Elite', cursive;
+    font-size: 12px;
+    background: url("/img/ricepaper2.png") 0 0 repeat;
+
+  *:focus
+    outline: none;
 </style>
